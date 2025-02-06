b1066ace8885b61bf19a4dd44c4128129ea832ac
luzivangois <luzivangois@outlook.com>
2025-02-06 16:49:22 -0300

    teste para exibir valor do cookie

--- src/app/attack/attack.component.ts
+++ src/app/attack/attack.component.ts
@@ -24,6 +24,7 @@ export class AttackComponent {
 
 getCookie(name: string): string | null {
   const value = `; ${document.cookie}`;
+  console.log(value);
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
   return null;
